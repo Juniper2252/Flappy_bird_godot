6bd962c1e254b135c831c581d8d11e3989f3a17f
Juniper2252 <142811426+Juniper2252@users.noreply.github.com>
2025-12-09 21:44:02 +0000

    final build 09/12

--- flappy-bird/scenes/pipes/pipe_spawner.gd
+++ flappy-bird/scenes/pipes/pipe_spawner.gd
@@ -18,7 +18,7 @@ func _on_spawn_rate_timeout() -> void:
 	$spawnRate.wait_time = rng.randf_range(1,3)
 	
 	
-	var num = rng.randfn(0.5,1)
+	var num = rng.randfn(0.5,0.5)
 	%PathFollow2D.progress_ratio = num
 	var instance = pipes.instantiate()
 	instance.global_position = %PathFollow2D.global_position
